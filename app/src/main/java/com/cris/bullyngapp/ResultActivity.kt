e01ab2712010c1f5649f4408b5cead9a5a61b9a4
Cris <crisuvas@gmail.com>
2018-04-16 08:54:30 -0500

    commit

--- app/src/main/java/com/cris/bullyngapp/ResultActivity.kt
+++ app/src/main/java/com/cris/bullyngapp/ResultActivity.kt
@@ -8,7 +8,6 @@ class ResultActivity : AppCompatActivity() {
     private var txtViewPercentage: TextView? = null
     private var txtViewDescription: TextView? = null
     private var txtViewBullyingLevel: TextView? = null
-
     private var points = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,25 +16,40 @@ class ResultActivity : AppCompatActivity() {
         txtViewPercentage = findViewById(R.id.textViewPercentage)
         txtViewDescription = findViewById(R.id.textViewDescription)
         txtViewBullyingLevel = findViewById(R.id.textViewBullyingLevel)
-        givePoints()
-        description()
+        givePoints(points)
+        description(points)
     }
-    fun givePoints(){
-        points = intent.getIntExtra("totalPoint",0)
-        points /= 20
-        txtViewPercentage?.text = "${points} %"
-        if (points == 0){
-            txtViewBullyingLevel?.text = "Nada de Bullying"
-        }else if(points <= 25){
-            txtViewBullyingLevel?.text = "Bullying Leve"
-        }else if(points <= 50){
-            txtViewBullyingLevel?.text = "Bullying Moderado"
-        }else if(points <=75){
-            txtViewBullyingLevel?.text = "Bullying Alto"
-        }else if(points <=100){
-            txtViewBullyingLevel?.text = "Bullying Grave"
+    fun givePoints(points: Int): Int {
+        this.points = intent.getIntExtra("totalPoint",0)
+        this.points /= 20
+        txtViewPercentage?.text = "${this.points} %"
+        when(this.points){
+            0 ->
+                txtViewDescription?.text = "Nada de Bullying"
+            in 1..25 ->
+                txtViewDescription?.text = "Bullying Leve"
+            in 26..50 ->
+                txtViewDescription?.text = "Bullying Moderado"
+            in 51..75 ->
+                txtViewDescription?.text = "Bullying Alto"
+            in 75..100 ->
+                txtViewDescription?.text = "Bullying Grave"
         }
+        return points
     }
-    fun description(){
+    fun description(points: Int) {
+        //MODIFICAR ESTO CON LAS RECOMENDACIONES ADECUADAS
+        when(points){
+            0 ->
+                txtViewBullyingLevel?.text = "Nada de Bullying"
+            in 1..25 ->
+                txtViewBullyingLevel?.text = "Bullying Leve"
+            in 26..50 ->
+                txtViewBullyingLevel?.text = "Bullying Moderado"
+            in 51..75 ->
+                txtViewBullyingLevel?.text = "Bullying Alto"
+            in 75..100 ->
+                txtViewBullyingLevel?.text = "Bullying Grave"
+        }
     }
 }
